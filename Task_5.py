b3b05b8716355c09c92e1acc047bd8edd2a6e0b3
Funt <funt_88@mail.ru>
2023-08-21 23:13:21 +0300

    bug fix

--- Task_5.py
+++ Task_5.py
@@ -1,6 +1,16 @@
-# Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами. 
+# Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами.
 # В результирующем списке не должно быть дубликатов.
+def find_duplicates(input_list):
+    counter = {}
+    duplicates = []
+    for element in input_list:
+        counter[element] = counter.get(element, 0) + 1
+    for element, count in counter.items():
+        if count > 1:
+            duplicates.append(element)
+    return duplicates
+
+
 input_list = [1, 2, 2, 3, 4, 4, 5, 5, 5]
-unique_set = set(input_list)
-result_list = list(unique_set)
-print(result_list)
+result = find_duplicates(input_list)
+print(result)
